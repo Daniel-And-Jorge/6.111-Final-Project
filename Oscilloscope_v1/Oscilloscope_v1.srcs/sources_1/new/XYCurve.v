385a336d24747aab1f81a42708923c10721a25fa
Daniel D Richman <ddr@mit.edu>
2016-12-05 20:26:29 -0500

    Older changes

--- Oscilloscope_v1/Oscilloscope_v1.srcs/sources_1/new/XYCurve.v
+++ Oscilloscope_v1/Oscilloscope_v1.srcs/sources_1/new/XYCurve.v
@@ -21,9 +21,10 @@
 
 `include "VerticalScaler.vh"
 
-/*
+
 module XYCurve
     #(parameter DATA_IN_BITS = 12,
+                SCALE_FACTOR_BITS = 10,
                 DISPLAY_X_BITS = 12,
                 DISPLAY_Y_BITS = 12,
                 RGB_COLOR = 12'hFF0,  //yellow
@@ -32,6 +33,7 @@ module XYCurve
                 DISPLAY_HEIGHT = 1024,
                 REAL_DISPLAY_WIDTH = 1688,
                 REAL_DISPLAY_HEIGHT = 1066,
+                WIDTH_ZERO_PIXEL = DISPLAY_WIDTH/2,
                 HEIGHT_ZERO_PIXEL = DISPLAY_HEIGHT/2,
                 ADDITIONAL_WAVE_PIXELS = 1,  //number of colored pixels below and on top of the actual wave
                 ADDRESS_BITS = 12
@@ -39,7 +41,7 @@ module XYCurve
     (input clock,
     input signed [DATA_IN_BITS-1:0] dataIn1,
     input signed [DATA_IN_BITS-1:0] dataIn2,
-    input signed [4:0] verticalShiftLeftFactor, // to allow for shrinking and growing the signal
+    input [SCALE_FACTOR_BITS-1:0] verticalScaleFactorTimes8, // to allow for shrinking and growing the signal
     input [DISPLAY_X_BITS-1:0] displayX,
     input [DISPLAY_Y_BITS-1:0] displayY,
     input hsync,
@@ -48,8 +50,8 @@ module XYCurve
     input [RGB_BITS-1:0] previousPixel,
     output [RGB_BITS-1:0] pixel,
     output reg drawStarting,
-    output reg [ADDRESS_BITS-1:0] address1,
-    output reg [ADDRESS_BITS-1:0] address2,
+    output [ADDRESS_BITS-1:0] address1,
+    output [ADDRESS_BITS-1:0] address2,
     output reg [DISPLAY_X_BITS-1:0] curveDisplayX,
     output reg [DISPLAY_Y_BITS-1:0] curveDisplayY,
     output reg curveHsync,
@@ -57,19 +59,24 @@ module XYCurve
     output reg curveBlank
     );
     
+    reg [DISPLAY_X_BITS-1:0] currentSample = 0;
+    wire [DATA_IN_BITS-1:0]shiftedPixel1;
+    wire [DATA_IN_BITS-1:0]shiftedPixel2;
+    
+    assign address1 = currentSample;
+    assign address2 = currentSample;
+    
+    assign shiftedPixel1 = WIDTH_ZERO_PIXEL + dataIn1; // x location
+    assign shiftedPixel2 = HEIGHT_ZERO_PIXEL - dataIn2; // y location, - since screen values increase downward
+    
     reg displayedRow = 0;
     reg [DISPLAY_X_BITS-1:0]pixelRows[1:0];
-    
-    //scale dataIn
-    wire signed [DATA_IN_BITS-1:0] scaledDataIn1;
-    assign scaledDataIn1 = `VERTICAL_SCALE(dataIn1, verticalShiftLeftFactor);
-    
-    wire signed [DATA_IN_BITS-1:0] scaledDataIn2;
-    assign scaledDataIn2 = `VERTICAL_SCALE(dataIn2, verticalShiftLeftFactor);
     
     reg pixelOn;
     
     always @(posedge clock) begin
+        // 1. Perform display from non-active row buffer
+    
         // TODO delay these by the correct number of cycles
         curveHsync <= hsync;
         curveVsync <= vsync;
@@ -84,35 +91,22 @@ module XYCurve
             drawStarting <= 0;
         end
         
-        //control pixel
+        // output pixel value from the buffer
+        pixelOn <= pixelRows[displayedRow][displayY];
+
+        // 2. Figure out what pixels will go in the next row
         // plan: we compute what to display row by row, since each row contains n samples
         // n clock cycles before we need to start displaying the pixels for a given row, we start requesting
         // samples from the buffer, iterating through time. Each timestep, we draw a pixel if necessary in
         // whatever location it should appear in in the next row. Meanwhile we're sending the current row
         // to the monitor. At the end of the row, the two swap.
+       
+       if ( shiftedPixel2 == (displayY + 1) )
+            pixelRows[~displayedRow][shiftedPixel1] <= 1;
         
-       if ( inputX == activeX && inputY == activeY )
-            pixelRows[~displayedRow][activeX] <= 1;
-        
-        // data value above zero
-        if ( (dataScreenLocation - ADDITIONAL_WAVE_PIXELS)<=displayY &&
-             displayY<=(dataScreenLocation + ADDITIONAL_WAVE_PIXELS) ) begin
-            pixelOn <= 1;
-        end else begin
-            pixelOn <= 0;
-        end
-        
-        //control address
-        if (0<=displayX && displayX < DISPLAY_WIDTH) begin
-            address1 <= displayX;
-            address2 <= displayX;
-        end
-        
-        // output pixel value from the buffer
-        pixelOn <= displayedRow ? pixelRow1[displayY] : pixelRow0[displayY];
+        currentSample <= currentSample + 1;
     end
     
     assign pixel = pixelOn ? RGB_COLOR : previousPixel;
     
 endmodule
-*/
